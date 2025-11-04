05a4700d9980ce5d234d9795f5d1bd83b93a9318
おもち <k.yamanaka0201@gmail.com>
2025-11-04 14:09:06 +0900

    Update urls.py

--- shift_manager/urls.py
+++ shift_manager/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from shifts import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shifts.urls')),  # すべてのアプリ URL を委譲
+
 ]
